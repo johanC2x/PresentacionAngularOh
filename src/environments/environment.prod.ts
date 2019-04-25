2012b3e1e57935cce459477fd291ebbba14695eb
Johan Cañari <johanc.cca@gmail.com>
2019-04-24 23:47:55 -0500

    AGREGANDO FIREBASE HOSTING

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBuJbXJLy6fXHNiQYkvTw3ir9FgVblTYX8",
+    authDomain: "inlaid-span-235315.firebaseapp.com",
+    databaseURL: "https://inlaid-span-235315.firebaseio.com",
+    projectId: "inlaid-span-235315",
+    storageBucket: "inlaid-span-235315.appspot.com",
+    messagingSenderId: "1044802615554"
+  }
 };
